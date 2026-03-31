d3db829c49f4d0562f4429f626f95e77753919d0
laibamhl2222-byte <laibamhl2222@gmail.com>
2026-03-31 11:40:30 +0500

    Update DATA STRUCTURE LAB 9 Q 2.cpp

--- DATA STRUCTURE LAB 9 Q 2.cpp
+++ DATA STRUCTURE LAB 9 Q 2.cpp
@@ -1,80 +1,59 @@
 #include <iostream>
-#include <queue>
-#include <stack>
 using namespace std;
 
-class GarageSystem {
-private:
-    queue<int> road;
-    stack<int> garage;
-public:
-    void onRoad(int id) {
-        road.push(id);
-    }
-
-    void showRoad() {
-        cout << "Road Vehicles: ";
-        queue<int> temp = road;
-        while (!temp.empty()) {
-            cout << temp.front() << " ";
-            temp.pop();
-        }
-        cout << endl;
-    }
-
-    void showGarage() {
-        cout << "Garage Vehicles: ";
-        stack<int> temp = garage;
-        if (temp.empty()) cout << "Empty";
-        while (!temp.empty()) {
-            cout << temp.top() << " ";
-            temp.pop();
-        }
-        cout << endl;
-    }
-
-    void enterGarage(int id) {
-        if (road.empty() || road.front() != id) {
-            cout << "Error: " << id << " cannot enter garage.\n";
-            return;
-        }
-        road.pop();
-        garage.push(id);
-        cout << id << " entered garage.\n";
-    }
-
-    void exitGarage(int id) {
-        if (garage.empty() || garage.top() != id) {
-            cout << "Error: " << id << " cannot exit garage.\n";
-            return;
-        }
-        cout << id << " leaving garage.\n";
-        garage.pop();
-    }
+struct Node {
+    int data;
+    Node* next;
 };
 
+Node* front = nullptr;
+Node* rear = nullptr;
+
+void enqueue(int value) {
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->next = nullptr;
+    if (rear == nullptr) {
+        front = rear = newNode;
+    } else {
+        rear->next = newNode;
+        rear = newNode;
+    }
+    cout << value << " inserted into queue.\n";
+}
+
+void dequeue() {
+    if (front == nullptr) {
+        cout << "Queue Underflow!\n";
+        return;
+    }
+    Node* temp = front;
+    cout << front->data << " removed from queue.\n";
+    front = front->next;
+    if (front == nullptr) rear = nullptr;
+    delete temp;
+}
+
+void display() {
+    if (front == nullptr) {
+        cout << "Queue is empty.\n";
+        return;
+    }
+    cout << "Queue elements: ";
+    Node* temp = front;
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
 int main() {
-    GarageSystem g;
-    g.onRoad(2);
-    g.onRoad(5);
-    g.onRoad(10);
-    g.onRoad(9);
-    g.onRoad(22);
-
-    g.showRoad();
-    g.showGarage();
-
-    g.enterGarage(2);
-    g.showRoad();
-    g.showGarage();
-
-    g.enterGarage(10); 
-    g.enterGarage(5);
-
-    g.exitGarage(2);   
-    g.exitGarage(5);
-
-    g.showGarage();
-
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    display();
+    dequeue();
+    display();
     return 0;
 }
